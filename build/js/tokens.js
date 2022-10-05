23e8c7bd5a51d5a5a1056154992e3a7f718f7565
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-10-05 05:54:28 -0300

    :tada: Importação Tokens do Figma

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 05 Oct 2022 08:52:08 GMT
+ */
+
+module.exports = {
+  "ColorDefaultWhite": "#ffffff",
+  "ColorPurple500": "#7159c1",
+  "ColorGray300": "#c1c1c4",
+  "ColorGray400": "#494958",
+  "ColorGray700": "#2a2a30",
+  "ColorGray900": "#121214"
+};
